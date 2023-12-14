17c7dfa7ee813849dad35b58d63c5fb851ec3234
Dan Yankowsky <bytecrafter@gmail.com>
2023-12-13 22:46:43 -0800

    Day14 Part2

--- src/test/java/org/balefrost/aoc2023/Day14.kt
+++ src/test/java/org/balefrost/aoc2023/Day14.kt
@@ -23,22 +23,85 @@ class Day14 {
 
     val lines = inputText.lines().dropLastWhile { it.isEmpty() }
 
-    fun moveRocks(rocks: List<List<Char>>): List<List<Char>> {
-        val result = rocks.mapTo(mutableListOf<MutableList<Char>>()) { it.toMutableList() }
-        for (r in 1..result.lastIndex) {
-            val row = result[r]
-            for (c in row.indices) {
-                if (row[c] == 'O') {
-                    var r2 = r - 1
-                    while (r2 >= 0) {
-                        if (result[r2][c] != '.') {
-                            break
-                        }
-                        --r2
+    enum class Direction(val dx: Int, val dy: Int) {
+        North(0, -1),
+        West(-1, 0),
+        South(0, 1),
+        East(1, 0);
+
+        val dir: Dir2D get() = Dir2D(dx, dy)
+    }
+
+    data class Point2D(val x: Int, val y: Int) {
+        operator fun plus(dir: Dir2D): Point2D {
+            return Point2D(x + dir.dx, y + dir.dy)
+        }
+
+        operator fun minus(dir: Dir2D): Point2D {
+            return Point2D(x - dir.dx, y - dir.dy)
+        }
+    }
+
+    data class Dir2D(val dx: Int, val dy: Int) {
+        operator fun plus(pt: Point2D): Point2D {
+            return pt + this
+        }
+    }
+
+    data class Region2D(val x: Int, val y: Int, val w: Int, val h: Int) {
+        operator fun contains(pt: Point2D): Boolean {
+            return pt.x >= x && pt.x < x + w && pt.y >= y && pt.y < y + h
+        }
+    }
+
+    fun iterationOrder(w: Int, h: Int, dir: Direction): Sequence<Point2D> {
+        return when(dir) {
+            Direction.North -> sequence {
+                for (r in 0 until h) {
+                    for (c in 0 until w) {
+                        yield(Point2D(c, r))
                     }
-                    result[r][c] = '.'
-                    result[r2 + 1][c] = 'O'
                 }
+            }
+            Direction.West -> sequence {
+                for (c in 0 until w) {
+                    for (r in 0 until h) {
+                        yield(Point2D(c, r))
+                    }
+                }
+            }
+            Direction.South -> sequence {
+                for (r in h - 1 downTo 0) {
+                    for (c in 0 until w) {
+                        yield(Point2D(c, r))
+                    }
+                }
+            }
+            Direction.East -> sequence {
+                for (c in w - 1 downTo 0) {
+                    for (r in 0 until h) {
+                        yield(Point2D(c, r))
+                    }
+                }
+            }
+        }
+    }
+
+    fun moveRocks(rocks: List<List<Char>>, dir: Direction): List<List<Char>> {
+        val result = rocks.mapTo(mutableListOf()) { it.toMutableList() }
+        val rgn = Region2D(0, 0, rocks.first().size, rocks.size)
+        iterationOrder(rocks.first().size, rocks.size, dir).forEach { (c, r) ->
+            if (result[r][c] == 'O') {
+                var dest = Point2D(c, r) + dir.dir
+                while (dest in rgn) {
+                    if (result[dest.y][dest.x] != '.') {
+                        break
+                    }
+                    dest = dest + dir.dir
+                }
+                dest = dest - dir.dir
+                result[r][c] = '.'
+                result[dest.y][dest.x] = 'O'
             }
         }
         return result
@@ -57,19 +120,54 @@ class Day14 {
         return totalLoad
     }
 
+    fun printRocks(rocks: List<List<Char>>) {
+        for (row in rocks) {
+            for (c in row) {
+                print(c)
+            }
+            println()
+        }
+    }
+
     @Test
     fun part01() {
-        lines.forEach(::println)
-        println()
-        val moved = moveRocks(lines.map { it.toList()})
-        moved.forEach {
-            println(it.joinToString(""))
-        }
+        val moved = moveRocks(lines.map { it.toList()}, Direction.North)
         println(computeLoad(moved))
+    }
+
+    fun tumble(rocks: List<List<Char>>): List<List<Char>> {
+        var r = rocks
+        r = moveRocks(r, Direction.North)
+        r = moveRocks(r, Direction.West)
+        r = moveRocks(r, Direction.South)
+        r = moveRocks(r, Direction.East)
+        return r
+    }
+
+    fun tumbleTimes(rocks: List<List<Char>>, count: Long): List<List<Char>> {
+        val tumbles = generateSequence(rocks, ::tumble)
+        val configurationToIndex = mutableMapOf<List<List<Char>>, Int>()
+        val configurations = mutableListOf<List<List<Char>>>()
+        for ((idx, tumble) in tumbles.withIndex()) {
+            configurations += tumble
+            val existingIndex = configurationToIndex.putIfAbsent(tumble, idx)
+            if (existingIndex != null) {
+                val prefixLength = existingIndex
+                val cycleLength = idx - existingIndex
+
+
+                val finalIdx = (count - prefixLength).mod(cycleLength) + prefixLength
+                return configurations[finalIdx]
+            }
+        }
+        error("Impossible")
     }
 
     @Test
     fun part02() {
+        val initial = lines.map { it.toList() }
+        val final = tumbleTimes(initial, 1000000000)
+        println(computeLoad(final))
     }
 
 }
